aadf8fa11f7bfedbe3f339430a3f17098d5e393d
“t-gen” <mamekeru202@gmail.com>
2020-09-03 07:58:33 +0900

    Add login / logout link

--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -28,8 +28,12 @@ public class TopPageIndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(request.getSession().getAttribute("flush") != null){
+            request.setAttribute("flush",request.getSession().getAttribute ("flash"));
+            request.getSession().removeAttribute("flash");
+        }
+
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/topPage/index.jsp");
-        response.getWriter().append("Served at: ").append(request.getContextPath());
         rd.forward(request, response);
     }
 
